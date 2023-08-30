51233baedf7d7cc3dd4247c902bb90a6f8139e90
sakayoshi <rassy.chrono13@gmail.com>
2023-08-30 22:36:48 +0900

    Update 081_a.py

--- abc/a/081_a.py
+++ abc/a/081_a.py
@@ -1,13 +1,1 @@
 print(input().count("1"))
-
-
-#print(sum(map(int, list(input()))))
-
-
-#s = input()
-#count = 0
-#for i in range(len(s)):
-#    a = int(s[i])
-#    if a == 1 :
-#        count += 1
-#print(count)
