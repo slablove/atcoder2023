3c31b956a2989f59c4b21487f2026f487971678a
sakayoshi <rassy.chrono13@gmail.com>
2023-09-05 19:42:47 +0900

    Update 179_b.py

--- abc/b/179_b.py
+++ abc/b/179_b.py
@@ -1,10 +1,12 @@
 k = int(input())
 count=0
+ans="No"
 for i in range(k):
   a,b = map(int,input().split())
   if a == b:
     count += 1
-if count >= 3:
-  print("Yes")
-else:
-  print("No")
+  else:
+    count = 0
+  if count == 3:
+    ans="Yes"
+print(ans)
